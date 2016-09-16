e9ff78b1cdc70e30b7973ed5f1ef5195881ed357
max <hordijchuk.m.i@gmail.com>
2016-09-16 13:34:11 +0300

    Add course 5 module 1 solution.

---
+++ course05/module1-solution/js/app.js
@@ -0,0 +1,41 @@
+(function (){
+  'use strict';
+
+  var app = angular.module("LunchCheck", []);
+
+  app.controller("LunchCheckController", LunchCheckController);
+
+  LunchCheckController.$inject = ["$scope"];
+  function LunchCheckController ($scope) {
+    $scope.check = function () {
+      var dishesCount = checkDishesCount($scope.lunch);
+      if (dishesCount == 0) {
+        $scope.message = "Please enter data first";
+        $scope.messageColor = "red";
+        $scope.border = "1px solid red";
+      } else {
+        $scope.messageColor = "green";
+        $scope.border = "1px solid green";
+        if (dishesCount <= 3) {
+            $scope.message = "Enjoy!";
+        } else {
+          $scope.message = "Too much!";
+        }
+      }
+    }
+  }
+
+  function checkDishesCount(lunchString) {
+    if (lunchString === undefined || lunchString === "") {
+      return 0;
+    }
+    var dishes = lunchString.split(",");
+    var dishesCount = 0;
+    for (var i in dishes) {
+      if (dishes[i].trim() !== '') {
+        dishesCount++;
+      }
+    }
+    return dishesCount;
+  }
+})();
