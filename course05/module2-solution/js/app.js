6a59c8280f2700ac370c60257fd4274a34ae695e
max <hordijchuk.m.i@gmail.com>
2016-09-27 16:37:59 +0300

    Add course 5 module 2 solution.

---
+++ course05/module2-solution/js/app.js
@@ -0,0 +1,47 @@
+(function (){
+  "use strict";
+
+  var app = angular.module("ShoppingListCheckOff", []);
+
+  app.service('ShoppingListCheckOffService', function () {
+    var service = this;
+
+    var buy = [
+      { name: "cookies", quantity: 10},
+      { name: "apples", quantity: 4},
+      { name: "tomatoes", quantity: 7},
+      { name: "lemons", quantity: 2},
+      { name: "ice creams", quantity: 2},
+      { name: "potatoes", quantity: 15},
+    ];
+    var bought = [];
+
+    service.getBuyItems = function () {
+      return buy;
+    };
+
+    service.getBoughtItems = function () {
+      return bought;
+    };
+
+    service.buyItem = function (index) {
+      bought.push(buy[index]);
+      buy.splice(index, 1);
+    }
+
+    return this;
+  });
+
+  app.controller("ToBuyController", ["ShoppingListCheckOffService", function (ShoppingListCheckOffService) {
+    var vm = this;
+    vm.items = ShoppingListCheckOffService.getBuyItems();
+    vm.buyItem = function (index) {
+      ShoppingListCheckOffService.buyItem(index);
+    };
+  }]);
+
+  app.controller("AlreadyBoughtController", ["ShoppingListCheckOffService", function (ShoppingListCheckOffService) {
+    var vm = this;
+    vm.items = ShoppingListCheckOffService.getBoughtItems();
+  }]);
+})();
