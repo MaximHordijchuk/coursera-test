981f23e674977f92fe7f68b971165edb8d45f74a
max <hordijchuk.m.i@gmail.com>
2016-09-29 22:13:24 +0300

    Add course 5 module 3 solution.

---
+++ course05/module3-solution/js/app.js
@@ -0,0 +1,66 @@
+(function (){
+  "use strict";
+
+  var app = angular.module("NarrowItDownApp", []);
+
+  app.service("MenuSearchService", ["$http", function ($http) {
+    var service = this;
+
+    service.getMatchedMenuItems = function(searchTerm) {
+      return $http({
+        method: "GET",
+        url: "https://davids-restaurant.herokuapp.com/menu_items.json"
+      }).then(function (result) {
+        // process result and only keep items that match
+        var foundItems = result.data.menu_items.filter(function (item) {
+          return item.description.includes(searchTerm);
+        });
+
+        // return processed items
+        return foundItems;
+      });
+    };
+  }]);
+
+  app.directive("foundItems", [function () {
+    var ddo = {
+      restrict: "E",
+      templateUrl: "foundItemsTemplate.html",
+      transclude: true,
+      scope: {
+        foundItems: "<",
+        onRemove: "&",
+        message: "<?"        
+      }
+    };
+
+    return ddo;
+  }]);
+
+  app.controller("NarrowItDownController", ["MenuSearchService", function (MenuSearchService) {
+    var vm = this;
+
+    vm.searchTerm = "";
+    vm.found = MenuSearchService.getMatchedMenuItems("");
+    vm.userClicked = false;
+
+    vm.getMatchedMenuItems = function () {
+      vm.userClicked = true;
+
+      if (vm.searchTerm === "") {
+        vm.found = [];
+      } else {
+        var menuItemsPromise = MenuSearchService.getMatchedMenuItems(vm.searchTerm);
+        menuItemsPromise.then(function(foundItems) {
+          vm.found = foundItems;
+        }, function() {
+          vm.found = [];
+        });
+      }
+    };
+    
+    vm.removeItem = function (index) {
+      vm.found.splice(index, 1);
+    };
+  }]);
+})();
